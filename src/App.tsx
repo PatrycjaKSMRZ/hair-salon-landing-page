0d2e2622e740e1ff75625367156df699cdbef3f0
PatrycjaKSMRZ <patr.kusmierz@gmail.com>
2026-08-18 13:36:59 +0200

    feat: add App component skeleton with routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Gallery from './components/Gallery/Gallery';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import PriceList from './components/PriceList/PriceList';
+import Team from './components/Team/Team';
 
 
 
@@ -7,7 +15,18 @@ function App() {
 
   return (
     <>
-   <div> ppp</div>
+   <div>
+    <Header />
+    <main>
+      <section id="Hero"><Hero /></section>
+      <section id="About"><About /></section>
+      <section id="Gallery"><Gallery /></section>
+      <section id="PriceList"><PriceList /></section>
+      <section id="Team"><Team /></section>
+      <section id="Contact"><Contact /></section>
+      <section id="Footer"><Footer /></section>
+    </main>
+   </div>
     </>
   )
 }
