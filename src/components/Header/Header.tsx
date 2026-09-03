12d5bcf3c4feee0733d3009ddd7a82234a8c386c
PatiKSMRZ <patr.kusmierz@gmail.com>
2026-09-03 15:10:45 +0200

    style: enhance navbar with hover effects and CTA button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,19 +4,21 @@ import styles from './Header.module.css'
 function    Header() {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+    //? to jest funkcja, która przełącza stan menu z true na false i na odwrót
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen) 
+    //? ()=> funkcja strzałkowa - utwórz funkcje, która po wywołaniu wykona to, co jest po strzałce 
 
     return (
         <header className={styles.header}>
             <div className={styles.wrapper}>
                 <span className={styles.logo}>Salon strong hair <span>.</span> </span>
                 <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''} `}>
-                    <a href="">home</a>
-                    <a href="">cennik</a>
-                    <a href="">galeria</a>
-                    <a href="">zespół</a>
-                    <a href="">kontakt</a>
-                    <a href="">UMÓW WIZYTĘ</a>
+                    <a href="" onClick={() => setIsMenuOpen(false)}>home</a>
+                    <a href="" onClick={() => setIsMenuOpen(false)}>cennik</a>
+                    <a href="" onClick={() => setIsMenuOpen(false)}>galeria</a>
+                    <a href="" onClick={() => setIsMenuOpen(false)}>zespół</a>
+                    <a href="" onClick={() => setIsMenuOpen(false)}>kontakt</a>
+                    <a href="#Contact" onClick={() => setIsMenuOpen(false)} className={styles.ctaButton}>UMÓW WIZYTĘ</a>
                 </nav>
                 <button className={styles.hamburger} onClick={toggleMenu}>
                     <span></span>
